6e5a7e79a74ed16b4a795520b6585b2709656cbb
Amitaibu <amitai@gizra.com>
2013-05-08 16:30:06 +0300

    Add context.

--- plugins/spaces_override/context/SpacesOverrideContext.class.php
+++ plugins/spaces_override/context/SpacesOverrideContext.class.php
@@ -9,8 +9,8 @@ class SpacesOverrideContext extends SpacesOverrideBase {
 
   public function override(&$data = NULL) {
     foreach (array_keys(context_reactions()) as $plugin) {
-      if ($override = $this->get("{$context->name}:reaction:{$plugin}")) {
-        $context->reactions[$plugin] = $override;
+      if ($override = $this->get("{$data->name}:reaction:{$plugin}")) {
+        $data->reactions[$plugin] = $override;
       }
     }
   }
