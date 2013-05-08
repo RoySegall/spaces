a400015df75f919ea5934464cfe24a61273315ea
Amitaibu <amitai@gizra.com>
2013-05-08 15:43:43 +0300

    Add overrides plugins.

---
+++ plugins/spaces_override/variables/SpacesOverrideVariables.class.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @file
+ * Spaces override for Drupal core variables (i.e. global $conf).
+ */
+
+class SpacesOverrideVariables extends SpacesOverrideBase {
+
+  public function get($key, $default_value = NULL) {
+    if ($value = parent::get($key, NULL)) {
+      return $value;
+    }
+
+    return variable_get($key, $default_value);
+  }
+
+  public function override() {
+    global $conf;
+    $conf = drupal_array_merge_deep($conf, $this->space->settings['variables']);
+  }
+
+}
